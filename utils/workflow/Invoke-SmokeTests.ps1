cfc72bbf77df3f596a2598ebfa7a360485c7a3ef
James Garriss <52328727+james-garriss@users.noreply.github.com>
2025-02-18 15:52:50 -0500

    cleanup comments

--- utils/workflow/Invoke-SmokeTests.ps1
+++ utils/workflow/Invoke-SmokeTests.ps1
@@ -1,9 +1,9 @@
 function Invoke-SmokeTests {
     <#
         .SYNOPSIS
-            Runs the smoke tests for ScubaGear
+            Runs the smoke tests for ScubaGear.
         .PARAMETER TestTenants
-            Info on the tenants against which the smoke tests are conducted. info
+            Info about the tenants against which the smoke tests are conducted.
     #>
     [CmdletBinding()]
     param(
@@ -17,27 +17,22 @@ function Invoke-SmokeTests {
 
     # Access certificate functions
     . Testing/Functional/SmokeTest/SmokeTestUtils.ps1
-    # Install-SmokeTestExternalDependencies
-    # Note this is dupe code with this:
+    # Install Selenium
     . utils/workflow/Install-SeleniumForTesting.ps1
     Install-SeleniumForTesting
     # Install ScubaGear modules
     Import-Module -Name .\PowerShell\ScubaGear\ScubaGear.psd1
     Initialize-SCuBA
 
-    # Workaround for Selenium. Loading psm1 instead of psd1
-    # Import-Module -Name (Get-Module -Name Selenium -ListAvailable).Path -Force
-
     # ScubaGear currently requires the provisioning of a certificate for using a ServicePrinicpal, rather than
     # using Workload Identity Federation, which would ordinarily be preferred for calling Microsoft APIs from
     # GitHub actions.
-    # $TestContainers = @()
     ForEach ($TestTenantObj in $TestTenants) {
         $TestContainers = @()
         $Properties = Get-Member -InputObject $TestTenantObj -MemberType NoteProperty
         $TestTenant = $TestTenantObj | Select-Object -ExpandProperty $Properties.Name
         $OrgName = $TestTenant.DisplayName
-        Write-Warning "Testing tenant $OrgName"
+        Write-Warning "Testing tenant $OrgName..."
         $DomainName = $TestTenant.DomainName
         $AppId = $TestTenant.AppId
         $PlainTextPassword = $TestTenant.CertificatePassword
@@ -62,16 +57,5 @@ function Invoke-SmokeTests {
             Write-Warning "Failed to install certificate for $OrgName because..."
             Write-Warning $_
         }
-
-        # $TestContainers += New-PesterContainer `
-        #     -Path "Testing/Functional/SmokeTest/SmokeTest001.Tests.ps1" `
-        #     -Data @{ Thumbprint = $Thumbprint; Organization = $DomainName; AppId = $AppId; M365Environment = $M365Env }
-        # $TestContainers += New-PesterContainer `
-        #     -Path "Testing/Functional/SmokeTest/SmokeTest002.Tests.ps1" `
-        #     -Data @{ OrganizationDomain = $DomainName; OrganizationName = $OrgName }
     }
-
-    # Invoke-Pester -Container $TestContainers -Output Detailed
-
-    # Remove-MyCertificates
 }
